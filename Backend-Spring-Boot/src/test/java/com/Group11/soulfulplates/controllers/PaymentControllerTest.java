041497e181b3479b8c1ad477d6f8e9c893d751c2
Ovaiz Ali <ov778752@dal.ca>
2024-03-27 14:41:01 -0300

    Add Integration Tests

--- Backend-Spring-Boot/src/test/java/com/Group11/soulfulplates/controllers/PaymentControllerTest.java
+++ Backend-Spring-Boot/src/test/java/com/Group11/soulfulplates/controllers/PaymentControllerTest.java
@@ -1,6 +1,5 @@
 package com.Group11.soulfulplates.controllers;
 
-import com.Group11.soulfulplates.payload.request.CreatePaymentRequest;
 import com.Group11.soulfulplates.payload.request.PaymentFilterRequestBuyer;
 import com.Group11.soulfulplates.payload.request.PaymentFilterRequestSeller;
 import com.Group11.soulfulplates.payload.request.UpdatePaymentStatusRequest;
@@ -16,9 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -35,35 +32,7 @@ class PaymentControllerTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
     }
-
-    @Test
-    void createPaymentAndTransaction_Success() throws Exception {
-        // Given
-        CreatePaymentRequest request = new CreatePaymentRequest();
-        Map<String, Object> response = new HashMap<>();
-        when(paymentService.createPaymentAndTransaction(request)).thenReturn(response);
-
-        // When
-        ResponseEntity<?> responseEntity = paymentController.createPaymentAndTransaction(request);
-
-        // Then
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        verify(paymentService, times(1)).createPaymentAndTransaction(request);
-    }
-
-    @Test
-    void createPaymentAndTransaction_Failure() throws Exception {
-        // Given
-        CreatePaymentRequest request = new CreatePaymentRequest();
-        when(paymentService.createPaymentAndTransaction(request)).thenThrow(new RuntimeException("Error creating payment"));
-
-        // When
-        ResponseEntity<?> responseEntity = paymentController.createPaymentAndTransaction(request);
-
-        // Then
-        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-        verify(paymentService, times(1)).createPaymentAndTransaction(request);
-    }
+    
 
     @Test
     void updatePaymentStatus_Success() throws Exception {
