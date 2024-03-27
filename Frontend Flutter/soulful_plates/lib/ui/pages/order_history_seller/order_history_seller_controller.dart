2f08eda3e98f538f0a5e5703aed8744769f40ac6
Anjali Rachel Benjamin <an653262@dal.ca>
2024-03-27 18:53:34 -0300

    Order history UI and API integration

--- Frontend Flutter/soulful_plates/lib/ui/pages/order_history_seller/order_history_seller_controller.dart
+++ Frontend Flutter/soulful_plates/lib/ui/pages/order_history_seller/order_history_seller_controller.dart
@@ -1,10 +1,17 @@
+import '../../../app_singleton.dart';
+import '../../../constants/app_theme.dart';
 import '../../../constants/enums/view_state.dart';
 import '../../../controller/base_controller.dart';
-import '../../../model/data_model.dart';
+import '../../../model/order_detail_model.dart';
+import '../../../network/network_interfaces/end_points.dart';
+import '../../../network/network_interfaces/i_dio_singleton.dart';
+import '../../../network/network_utils/api_call.dart';
 import '../../../utils/pagination_utils.dart';
 
 class OrderHistorySellerController extends BaseController
-    with PaginationUtils<DataModel> {
+    with PaginationUtils<OrderDetailModel> {
+  OrderStatus orderStatus = OrderStatus.Pending;
+
   @override
   void onInit() {
     super.onInit();
@@ -19,7 +26,40 @@ class OrderHistorySellerController extends BaseController
     }
   }
 
-  void getDataFromAPI() async {}
+  void getDataFromAPI() async {
+    updateLoader(ViewStateEnum.busy);
+
+    var result = await ApiCall().call<OrderDetailModel>(
+        method: RequestMethod.getPost,
+        endPoint: EndPoints.getOrdersForStore,
+        apiCallType: ApiCallType.seller,
+        obj: OrderDetailModel(),
+        parameters: {
+          "storeId": AppSingleton.storeId,
+          // "userId": 1,
+          "status": OrderStatus.Completed.name,
+          "limit": recordsPerPage,
+          "offset": pageNo
+        }); //male api call here
+
+    if (result != null) {
+      List<OrderDetailModel> temp = result;
+      if (temp.isEmpty || temp.length < recordsPerPage) {
+        hasReachedMax = true;
+      }
+      if (pageNo == 0) {
+        dataList.clear();
+      }
+      if (temp.isNotEmpty) {
+        dataList.addAll(temp);
+      }
+      updateLoader(ViewStateEnum.idle);
+    } else {
+      dataList = [];
+      updateLoader(ViewStateEnum.idle);
+    }
+    update();
+  }
 
   @override
   bool isLoading() {
--- Frontend Flutter/soulful_plates/lib/ui/pages/order_history_seller/order_history_seller_screen.dart
+++ Frontend Flutter/soulful_plates/lib/ui/pages/order_history_seller/order_history_seller_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../../../constants/size_config.dart';
+import 'package:soulful_plates/Utils/Extensions.dart';
 
 import '../../../constants/app_colors.dart';
 import '../../../constants/app_sized_box.dart';
 import '../../../constants/app_text_styles.dart';
 import '../../../constants/enums/view_state.dart';
-import '../../../utils/extensions.dart';
+import '../../../constants/size_config.dart';
 import '../../widgets/base_common_widget.dart';
+import '../../widgets/order_item_widget.dart';
 import 'order_history_seller_controller.dart';
 
 class OrderHistorySellerScreen extends GetView<OrderHistorySellerController>
@@ -18,7 +19,7 @@ class OrderHistorySellerScreen extends GetView<OrderHistorySellerController>
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("OrderHistorySeller"),
+          title: const Text("Order History"),
         ),
         backgroundColor: AppColor.whiteColor,
         body: SafeArea(
@@ -33,84 +34,73 @@ class OrderHistorySellerScreen extends GetView<OrderHistorySellerController>
   }
 
   Widget getBody(BuildContext context) {
-    return Column(
-      children: [
-        12.rVerticalSizedBox(),
-        Expanded(
-          child: Stack(children: [
-            controller.dataList.isNotEmpty
-                ? RefreshIndicator(
-                    onRefresh: () async {
-                      controller.resetPagination();
+    return Stack(children: [
+      controller.dataList.isNotEmpty
+          ? RefreshIndicator(
+              onRefresh: () async {
+                controller.resetPagination();
+              },
+              child: NotificationListener<ScrollNotification>(
+                  onNotification: (scrollNotification) {
+                    if (scrollNotification.metrics.pixels >=
+                            scrollNotification.metrics.maxScrollExtent &&
+                        !controller.hasReachedMax &&
+                        !controller.isLoading()) {
+                      controller.pageNo = (controller.pageNo + 1);
+                      controller.loadMore();
+                    }
+                    return false;
+                  },
+                  child: ListView.builder(
+                    padding: EdgeInsets.zero,
+                    shrinkWrap: true,
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    itemCount: controller.dataList.length + 1,
+                    itemBuilder: (context, index) {
+                      if (index < controller.dataList.length) {
+                        return InkWell(
+                          onTap: () async {
+                            //todo tap on the item
+                          },
+                          child: OrderItemWidget(
+                                  orderDetailModel: controller.dataList[index])
+                              .paddingVertical8(),
+                        );
+                      } else if (controller.moreLoading == ViewStateEnum.busy) {
+                        return controller.loadMoreLoader(
+                            color: AppColor.blackColor);
+                      } else {
+                        return AppSizedBox.sizedBox0;
+                      }
                     },
-                    child: NotificationListener<ScrollNotification>(
-                        onNotification: (scrollNotification) {
-                          if (scrollNotification.metrics.pixels >=
-                                  scrollNotification.metrics.maxScrollExtent &&
-                              !controller.hasReachedMax &&
-                              !controller.isLoading()) {
-                            controller.pageNo = (controller.pageNo + 1);
-                            controller.loadMore();
-                          }
-                          return false;
-                        },
-                        child: ListView.separated(
-                          padding: EdgeInsets.zero,
-                          shrinkWrap: true,
-                          physics: const AlwaysScrollableScrollPhysics(),
-                          itemCount: controller.dataList.length + 1,
-                          separatorBuilder: (context, index) {
-                            return 2.rVerticalGreySizedBox();
-                          },
-                          itemBuilder: (context, index) {
-                            if (index < controller.dataList.length) {
-                              return InkWell(
-                                onTap: () async {
-                                  //todo tap on the item
-                                },
-                                child: //todo change widget with item widget
-                                    Text("Item number $index")
-                                        .paddingAllDefault(),
-                              );
-                            } else if (controller.moreLoading ==
-                                ViewStateEnum.busy) {
-                              return controller.loadMoreLoader(
-                                  color: AppColor.blackColor);
-                            } else {
-                              return AppSizedBox.sizedBox0;
-                            }
-                          },
-                        )),
-                  )
-                : Center(
-                    child: GestureDetector(
-                      behavior: HitTestBehavior.opaque,
-                      onTap: () {
-                        controller.resetPagination();
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.refresh_outlined,
-                            size: 24.rSize(),
-                            color: AppColor.primaryColor,
-                          ),
-                          Text(
-                            'No data available!',
-                            style: AppTextStyles.textStyleBlack16With400,
-                          ),
-                        ],
-                      ).paddingAll12(),
+                  )),
+            )
+          : Center(
+              child: GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () {
+                  controller.resetPagination();
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.refresh_outlined,
+                      size: 24.rSize(),
+                      color: AppColor.primaryColor,
                     ),
-                  ),
-            controller.state == ViewStateEnum.busy
-                ? const Center(child: CircularProgressIndicator())
-                : AppSizedBox.sizedBox0
-          ]).paddingSymmetricSide(vertical: 8, horizontal: 16),
-        ),
-      ],
-    );
+                    Text(
+                      'No data available!',
+                      style: AppTextStyles.textStyleBlack16With400,
+                    ),
+                  ],
+                ).paddingAll12(),
+              ),
+            ),
+      controller.state == ViewStateEnum.busy
+          ? const Center(child: CircularProgressIndicator())
+          : AppSizedBox.sizedBox0
+    ]).paddingAllDefault();
   }
 }
