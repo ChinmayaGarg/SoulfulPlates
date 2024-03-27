1f53e134e95eba88fc5f8c5a968bdee50d4401fc
Ovaiz Ali <ov778752@dal.ca>
2024-03-27 14:29:31 -0300

    Add Integration Tests

--- Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/repository/UserRepository.java
+++ Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/repository/UserRepository.java
@@ -1,14 +1,10 @@
 package com.Group11.soulfulplates.repository;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
+import com.Group11.soulfulplates.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.Group11.soulfulplates.models.User;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
